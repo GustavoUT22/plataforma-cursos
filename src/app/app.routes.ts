eaa7a2012f211a7778095e14b51bf3b9e9f83b60
aldair <casafrancadev@gmail.com>
2026-06-17 12:35:27 -0500

    agregar admin layout (dashboard, courses, students, teachers, solo front)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Home } from './features/home/home';
 import { CourseList } from './features/courses/course-list/course-list';
 import { CourseDetail } from './features/courses/course-detail/course-detail';
 import { CourseForm } from './features/courses/course-form/course-form';
+import { AdminDashboard } from './features/admin/admin-dashboard/admin-dashboard';
+import { AdminLayout } from './features/admin/admin-layout/admin-layout';
+import { AdminCourses } from './features/admin/admin-courses/admin-courses';
+import { AdminStudents } from './features/admin/admin-students/admin-students';
+import { AdminTeachers } from './features/admin/admin-teachers/admin-teachers';
 
 // definimos rutas de la aplicación
 /**
@@ -11,8 +16,15 @@ import { CourseForm } from './features/courses/course-form/course-form';
  */
 export const routes: Routes = [
   {
-    path: '',
-    component: Home,
+    path: 'admin',
+    component: AdminLayout,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: AdminDashboard },
+      { path: 'courses', component: AdminCourses },
+      { path: 'students', component: AdminStudents },
+      { path: 'teachers', component: AdminTeachers },
+    ],
   },
   {
     path: 'courses',
