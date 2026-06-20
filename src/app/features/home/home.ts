c2dd2ab7c3ab86b0c3ccf7e7404253101ff31af6
GustavoUT22 <gustavougartetorres@gmail.com>
2026-06-19 22:27:48 -0500

    add: header, footer and home page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CourseService } from '../../core/services/course.service';
+import { Course } from '../../shared/models/course.model';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  courses = signal<Course[]>([]);
+  private courseService = inject(CourseService);
+
+  ngOnInit(): void {
+    const allCourses = this.courseService.getCourses();
+    this.courses.set(allCourses.slice(0, 6));
+  }
+}
