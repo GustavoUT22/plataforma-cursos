c4cc9a740e6d99eecef789fda6fb16a95ab8b61d
aldair <casafrancadev@gmail.com>
2026-06-23 14:39:18 -0500

    Agregue en la tabla del panel de admin-teachers datos dinámicos

--- src/app/features/admin/admin-teachers/admin-teachers.ts
+++ src/app/features/admin/admin-teachers/admin-teachers.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UserModel } from '../../../shared/models/user.model';
+import { UserService } from '../../../core/services/user.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin-teachers',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './admin-teachers.html',
   styleUrl: './admin-teachers.css',
 })
-export class AdminTeachers {}
+export class AdminTeachers implements OnInit {
+  users = signal<UserModel[]>([]);
+
+  private userService = inject(UserService);
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        const teachers = data.filter((user) => user.role === 'teacher');
+
+        this.users.set(teachers);
+        console.log(data);
+      },
+      error: (error) => {
+        console.log(`Error to get teachers`, error);
+      },
+    });
+  }
+}
